57187f5a25e4fc7f748e1c32135540a8bf1c1f3b
ododonov <o.dodonov@avtomatika.kz>
2021-04-20 10:21:10 +0600

    adding files

---
+++ scripts/fullscreen.js
@@ -0,0 +1,37 @@
+var elem = document.documentElement
+let scr_count = false;
+
+function screen_change() {
+  if (!scr_count) {
+    openFullscreen();
+    scr_count = !scr_count;
+  } else {
+    closeFullscreen();
+    scr_count = !scr_count;
+  }
+}
+
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.mozCancelFullScreen) { /* Firefox */
+    document.mozCancelFullScreen();
+  } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE/Edge */
+    document.msExitFullscreen();
+  }
+}
